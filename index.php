711e2dbc142fe703f8d2dcdc09853e8ba2dd1390
Florent-Vasseur <vasseurflorent@gmail.com>
2022-10-20 22:05:26 +0200

    ok

--- index.php
+++ index.php
@@ -4,16 +4,27 @@ require __DIR__ . '/vendor/autoload.php';
 
 use CowSay\Cow;
 
-$Marguerite = new Cow('Hello, Farm!');
+$Marguerite = new Cow('Suprise MotherFucker!');
 
 // store the output in a variable
-$Marguerite->setEyes('oO')
+$Marguerite->setEyes('$$')
     ->setTongue('U')
     ->setPoop('@@@')
     ->setUdder('W');
 
 
 echo $Marguerite . PHP_EOL;
+
+$bear = new CowSay\Bear("Oh, oh, Tiens voilà quelqu'un Petit Ours Brun");
+
+$sheep = new CowSay\Sheep("bééééééééééééééééééééé");
+
+$tux = new CowSay\Tux("Linus Torvalds is my father !");
+
+
+echo $bear . PHP_EOL;
+echo $sheep . PHP_EOL;
+echo $tux . PHP_EOL;
 
 
 ?>
